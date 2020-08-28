3a937c3e62df566cb0d18ff57b46d9c2b747ccfe
angusmf <angusmf@gmail.com>
2020-08-28 12:29:05 -0700

    add navigation buttons

--- Assets/Scripts/Wiki/WikiPanel.cs
+++ Assets/Scripts/Wiki/WikiPanel.cs
@@ -1,33 +1,59 @@
 ﻿using markdown;
+using System.Collections.Generic;
+
+
 
 using Unity.UIWidgets.engine;
 using Unity.UIWidgets.material;
 using Unity.UIWidgets.widgets;
+using System;
+using Unity.UIWidgets.rendering;
+using Unity.UIWidgets.painting;
 
 namespace TownWorldWiki
 {
     public class WikiPanel : UIWidgetsPanel
     {
 
-        private Markdown markdown;
-        private AppBar appBar;
+        private Stack<string> m_navHistory = new Stack<string>();
+
+        private string m_appBarTitle;
         private string markdownData1;
 
-        private string title = "Markdown Demo";
+        private string title = "Town World Wiki";
+        private string m_wikiRoot = "./Town_World";
+        private string m_currURL;
 
-        private void buildMarkdown()
+        protected override void Awake()
         {
-            markdown = new Markdown(null, markdownData1,
-                      syntaxHighlighter: new DartSyntaxHighlighter(SyntaxHighlighterStyle.lightThemeStyle()),
-                      onTapLink: handleTap
-                      );
+            if (string.IsNullOrWhiteSpace(m_appBarTitle)) m_appBarTitle = "Some title";
+            if (string.IsNullOrEmpty(markdownData1)) markdownData1 = new MarkdownData().Data;
         }
 
-        private void handleTap(string url)
+        private void HandleTap(string url)
         {
-            appBar = new AppBar(title: new Unity.UIWidgets.widgets.Text("Some NEW!!! title"));
-            markdownData1 = new MarkdownData2().Data;
+            if (!HandleInternalLink(url))
+                HandleExternalLink(url);  
+        }
+
+        private bool HandleInternalLink(string url)
+        {
+            if (!url.StartsWith(m_wikiRoot)) return false;
+
+
+            m_navHistory.Push(url);
+            m_appBarTitle = url == m_wikiRoot ? "HOME" : "Some NEW!!! title";
+            markdownData1 = (url == m_currURL || url == m_wikiRoot) ? new MarkdownData().Data : new MarkdownData2().Data;
+
             recreateWidget();
+            m_currURL = url;
+
+            return true;
+        }
+
+        private void HandleExternalLink(string url)
+        {
+            throw new NotImplementedException();
         }
 
         protected override void OnEnable()
@@ -48,17 +74,32 @@ namespace TownWorldWiki
 
         protected override Widget createWidget()
         {
-            if (appBar == null) appBar = new AppBar(title: new Unity.UIWidgets.widgets.Text("Some title"));
-            if (string.IsNullOrEmpty(markdownData1)) markdownData1 = new MarkdownData().Data;
-            buildMarkdown();
-
             return new MaterialApp(
                 title: title,
                 home: new Scaffold(
-                    appBar: appBar,
-                    body: markdown
-                )
-            );
+                    appBar: new AppBar(title: new Text(m_appBarTitle)),
+
+                    body: new Markdown(data: markdownData1,
+                      syntaxHighlighter: new DartSyntaxHighlighter(SyntaxHighlighterStyle.lightThemeStyle()),
+                      onTapLink: HandleTap),
+
+                    persistentFooterButtons: new List<Widget>()
+                    { 
+                        RaisedButton.icon(
+                            icon: new Icon(Icons.home, size: 18.0f, color: new Unity.UIWidgets.ui.Color(0xFFFFFFFF)),
+                            label: new Text("HOME BUTTON", style: new TextStyle(true, new Unity.UIWidgets.ui.Color(0xFF000000))),
+                            onPressed: () => {
+                                // Perform some action
+                                HandleInternalLink(m_wikiRoot);
+                            }),
+                        RaisedButton.icon(
+                            icon: new Icon(Icons.arrow_back, size: 18.0f, color: new Unity.UIWidgets.ui.Color(0xFFFFFFFF)),
+                            label: new Text("BACK BUTTON", style: new TextStyle(true, new Unity.UIWidgets.ui.Color(0xFF000000))),
+                            onPressed: () => {
+                                // Perform some action
+                                HandleInternalLink(m_navHistory.Pop());
+                            }),
+                    }));
         }
     }
 }
