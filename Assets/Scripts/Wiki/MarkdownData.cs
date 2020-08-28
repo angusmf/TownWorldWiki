1333fa556211f8408cc2211a91a6e1cebaa9ff2e
angusmf <angusmf@gmail.com>
2020-08-28 09:13:47 -0700

    Add WikiPanel based on the example panel in the UIWidgets Markdown plugin. It demonstrates reloading the panel with new data by loading a second markdown example when any link is tapped.

---
+++ Assets/Scripts/Wiki/MarkdownData.cs
@@ -0,0 +1,70 @@
+﻿namespace TownWorldWiki
+{
+    public class MarkdownData
+    {
+		public MarkdownData(string data = null)
+        {
+			if (data != null) markdownData1 = data;
+        }
+
+		public string Data { get { return markdownData1; } }
+
+		private string markdownData1 = @"
+
+# Town World
+
+
+Summary
+=======
+Town World is somewhere in [Superspace](./Town_World/Locations/Superspace.markdown). It is a planet which has been [tidally locked](./Town_World/Concepts/Tidal_Lock.markdown) to its star at some point in the past. Time may not pass in the same manner here as in [normal space](./Town_World/Locations/Space.markdown), where Earth resides. It’s very difficult to cross between space and superspace. Additionally, the current time, and even the [timeline](./Town_World/Concepts/Timeline.markdown) of events, on the other side of each crossing, may be different than expected.
+
+It is planet of small towns with different mix of [creatures](./Town_World/Creatures.markdown) in each. They form [factions](./Town_World/Concepts/Factions.markdown) that may or may not cooperate. 
+
+
+
+*****
+
+
+Town World!
+===========
+
+[Locations](./Town_World/Locations.markdown)
+
+[Characters](./Town_World/Characters.markdown)
+
+[Creatures](./Town_World/Creatures.markdown)
+
+[Events](./Town_World/Events.markdown)
+
+[Resources](./Town_World/Resources.markdown)
+
+[Concepts](./Town_World/Concepts.markdown)
+
+[Vehicles](./Town_World/Vehicles.markdown)
+
+[Activities](./Town_World/Activities.markdown)
+
+[Hazards](./Town_World/Hazards.markdown)
+
+
+*****
+
+
+Game Stuff
+==========
+[Gameplay](./Town_World/Gameplay.markdown)
+
+[Guidelines For Town World Design](./Town_World/Design_Guide.markdown)
+
+Temporary Notes
+===============
+
+[Loose Ends](./Town_World/Loose_Ends.markdown)
+
+
+
+
+  
+";
+	}
+}
---
+++ Assets/Scripts/Wiki/WikiPanel.cs
@@ -0,0 +1,64 @@
+﻿using markdown;
+
+using Unity.UIWidgets.engine;
+using Unity.UIWidgets.material;
+using Unity.UIWidgets.widgets;
+
+namespace TownWorldWiki
+{
+    public class WikiPanel : UIWidgetsPanel
+    {
+
+        private Markdown markdown;
+        private AppBar appBar;
+        private string markdownData1;
+
+        private string title = "Markdown Demo";
+
+        private void buildMarkdown()
+        {
+            markdown = new Markdown(null, markdownData1,
+                      syntaxHighlighter: new DartSyntaxHighlighter(SyntaxHighlighterStyle.lightThemeStyle()),
+                      onTapLink: handleTap
+                      );
+        }
+
+        private void handleTap(string url)
+        {
+            appBar = new AppBar(title: new Unity.UIWidgets.widgets.Text("Some NEW!!! title"));
+            markdownData1 = new MarkdownData2().Data;
+            recreateWidget();
+        }
+
+        protected override void OnEnable()
+        {
+            // if you want to use your own font or font icons.   
+            // FontManager.instance.addFont(Resources.Load<Font>(path: "path to your font"), "font family name");
+
+            // load custom font with weight & style. The font weight & style corresponds to fontWeight, fontStyle of 
+            // a TextStyle object
+            // FontManager.instance.addFont(Resources.Load<Font>(path: "path to your font"), "Roboto", FontWeight.w500, 
+            //    FontStyle.italic);
+
+            // add material icons, familyName must be "Material Icons"
+            // FontManager.instance.addFont(Resources.Load<Font>(path: "path to material icons"), "Material Icons");
+
+            base.OnEnable();
+        }
+
+        protected override Widget createWidget()
+        {
+            if (appBar == null) appBar = new AppBar(title: new Unity.UIWidgets.widgets.Text("Some title"));
+            if (string.IsNullOrEmpty(markdownData1)) markdownData1 = new MarkdownData().Data;
+            buildMarkdown();
+
+            return new MaterialApp(
+                title: title,
+                home: new Scaffold(
+                    appBar: appBar,
+                    body: markdown
+                )
+            );
+        }
+    }
+}
