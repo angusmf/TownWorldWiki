28d2281a343c54c887651831f2d04f4aa085ac58
angusmf <angusmf@gmail.com>
2020-09-04 12:16:00 -0700

    son of typo

--- ci/build.sh
+++ ci/build.sh
@@ -8,7 +8,7 @@ echo "Building for $BUILD_TARGET"
 export BUILD_PATH=./Builds/$BUILD_TARGET/
 mkdir -p $BUILD_PATH
 /opt/Unity/Editor/Unity \
-  -nographics
+  -nographics \
   -projectPath $(pwd) \
   -quit \
   -batchmode \
