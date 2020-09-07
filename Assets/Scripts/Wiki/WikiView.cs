36e024eb987c76382a95d72121f16beb04b589f5
angusmf <angusmf@gmail.com>
2020-09-07 11:46:19 -0700

    fix webgl

--- Assets/Scripts/Wiki/WikiView.cs
+++ Assets/Scripts/Wiki/WikiView.cs
@@ -10,6 +10,9 @@ using Unity.UIWidgets.painting;
 using System;
 using System.IO;
 using System.Collections.Generic;
+using UnityEngine.Networking;
+
+using Cysharp.Threading.Tasks;
 
 namespace UIWidgetsWiki
 {
@@ -27,21 +30,23 @@ namespace UIWidgetsWiki
         string RootName = "Town_World";
 
         private const string MARKDOWN__FILE_EXTENSION = ".markdown";
+        private const string NO_DATA = "no data";
 
         private Stack<NavigationState> m_navHistory = new Stack<NavigationState>();
 
         private string m_appBarTitle = string.Empty;
-        private string markdownData1 = "no data";
+        private string markdownData1 = NO_DATA;
         private string m_currentDirectory = string.Empty;
         private MarkdownStyleSheet m_markdownStyleSheet;
         private PanelConfig m_config;
+        private HashSet<string> m_PathExists = new HashSet<string>();
 
-        protected override void Awake()
+        protected override async void Awake()
         {
             if (m_config == null) m_config = GetComponent<PanelConfig>();
             Title = m_config?.Title;
             RootName = m_config?.RootName;
-            LoadPage(Path.Combine(Application.streamingAssetsPath, "markdown"), RootName);
+            await LoadPageAsync(Path.Combine(Application.streamingAssetsPath, "markdown"), RootName + MARKDOWN__FILE_EXTENSION);
             m_markdownStyleSheet = MarkdownStyleSheet.fromTheme(new ThemeData(textTheme:
                 new TextTheme(
 
@@ -63,27 +68,20 @@ namespace UIWidgetsWiki
         }
         
 
-        private void HandleTap(string url)
+        private async void HandleTap(string url)
         {
-            if (!HandleInternalLink(url))
+            if (!await HandleInternalLink(url))
                 HandleExternalLink(url);  
         }
 
-        private bool HandleInternalLink(string url)
+        private async UniTask<bool> HandleInternalLink(string url)
         {
             if (!url.StartsWith("./") && !url.StartsWith("..")) return false;
 
-            string currentDirectory = Path.Combine(m_currentDirectory, Path.GetDirectoryName(url));
-
-            if (File.Exists(Path.Combine(currentDirectory, Path.GetFileName(url))))
-            {
-                PushCurrentPageToHistory();
-                ReloadPage(currentDirectory, Path.GetFileNameWithoutExtension(url));
-            }
-            else
-            {
-                Debug.LogWarningFormat("couldn't find file at path {0} (url = {1})",
-                    Path.Combine(currentDirectory, Path.GetFileName(url)),
+            if (!await NavigateTo(Path.Combine(m_currentDirectory, Path.GetDirectoryName(url)), Path.GetFileName(url)))
+            { 
+            Debug.LogWarningFormat("HandleInternalLink couldn't find file at path {0} (url = {1})",
+                    Path.Combine(m_currentDirectory, Path.GetDirectoryName(url)),
                     url);
                 //TODO user notifcation of missing file
                 //TODO user add new file
@@ -92,25 +90,48 @@ namespace UIWidgetsWiki
             return true;
         }
 
-        void ReloadPage(string path, string file)
+        async UniTask<bool> ReloadPage(string path, string file)
         {
-            LoadPage(path, file);
-            recreateWidget();
-        }
-
-        private void LoadPage(string path, string file)
-        {
-            if (!Application.isPlaying) return;
-            if (File.Exists(Path.Combine(path, file + MARKDOWN__FILE_EXTENSION)))
+            if (await LoadPageAsync(path, file))
             {
-                markdownData1 = File.ReadAllText(Path.Combine(path, file + MARKDOWN__FILE_EXTENSION)).Replace("![](./", "![](");
-
-                m_currentDirectory = path;
-                m_appBarTitle = file;
+                recreateWidget();
+                return true;
             }
             else
             {
-                Debug.LogErrorFormat("couldn't find file at path {0}. Real Error. This shouldn't happen", Path.Combine(path, file + MARKDOWN__FILE_EXTENSION));
+                return false;
+            }
+        }
+
+        private string loadPath;
+
+        private async UniTask<bool> LoadPageAsync(string path, string file)
+        {
+            if (!Application.isPlaying) return false;
+
+            loadPath = Path.Combine(path, file);
+
+#if (!UNITY_EDITOR && UNITY_WEBGL) || (!UNITY_EDITOR && UNITY_ANDROID )
+
+            markdownData1 = (await UnityWebRequest.Get(loadPath).SendWebRequest())
+                .downloadHandler.text?.Replace("![](./", "![](");
+
+            if (markdownData1 != null)
+            {
+#else
+            if (File.Exists(loadPath))
+            {
+                markdownData1 = File.ReadAllText(loadPath).Replace("![](./", "![](");
+#endif
+                if (!m_PathExists.Contains(loadPath)) m_PathExists.Add(loadPath);
+                m_currentDirectory = path;
+                m_appBarTitle = file;
+                return true;
+            }
+            else
+            {
+                Debug.LogErrorFormat("LoadPage couldn't find file at path {0}.", Path.Combine(path, file));
+                return false;
             }
         }
 
@@ -131,20 +152,56 @@ namespace UIWidgetsWiki
 
         private void PushCurrentPageToHistory()
         {
-            if (File.Exists(Path.Combine(m_currentDirectory, m_appBarTitle + MARKDOWN__FILE_EXTENSION)))
+            if (m_PathExists.Contains(Path.Combine(lastPath, lastFile)))
             {
-                m_navHistory.Push(new NavigationState() { directory = m_currentDirectory, page = m_appBarTitle });
+                m_navHistory.Push(new NavigationState() { directory = lastPath, page = lastFile });
             }
             else
             {
                 Debug.LogWarningFormat("Could not push non-existent page \"{0}\" to history.",
-                    Path.Combine(m_currentDirectory, m_appBarTitle + MARKDOWN__FILE_EXTENSION));
+                    Path.Combine(m_currentDirectory, m_appBarTitle));
             }
+        }
+
+        private string lastPath;
+        private string lastFile;
+
+        private async UniTask<bool> NavigateTo(string path, string file)
+        {
+            lastPath = m_currentDirectory;
+            lastFile = m_appBarTitle;
+
+            if (await ReloadPage(path, file))
+            {
+                PushCurrentPageToHistory();
+                return true;
+            }
+            return false;
+        }
+
+        async void Back()
+        {
+            if (m_navHistory.Count > 0)
+            {
+                NavigationState navigationState = m_navHistory.Pop();
+                await ReloadPage(navigationState.directory, navigationState.page);
+            }
+        }
+
+        async void Home()
+        {
+            await NavigateTo(Path.Combine(Application.streamingAssetsPath, "markdown"), RootName + MARKDOWN__FILE_EXTENSION);
         }
 
         protected override Widget createWidget()
         {
             if (!Application.isPlaying) return null;
+
+            if (markdownData1 == NO_DATA)
+            {
+                NavigateTo(Path.Combine(Application.streamingAssetsPath, "markdown"), RootName + MARKDOWN__FILE_EXTENSION);
+                return null;
+            }
 
             return new MaterialApp(
                 title: Title,
@@ -166,21 +223,12 @@ namespace UIWidgetsWiki
                         RaisedButton.icon(
                             icon: new Icon(Icons.home, size: 18.0f, color: Colors.white),
                             label: new Text("HOME", style: new TextStyle(true, color: Colors.white)),
-                            onPressed: () => {
-                                PushCurrentPageToHistory();
-                                ReloadPage(Path.Combine(Application.streamingAssetsPath, "markdown"),  RootName);
-                            }),
-                   
+                            onPressed: Home),
+
                         RaisedButton.icon(
                             icon: new Icon(Icons.arrow_back, size: 18.0f, color: Colors.white),
                             label: new Text("BACK", style: new TextStyle(true, color: Colors.white)),
-                            onPressed: () => {
-                                if (m_navHistory.Count > 0)
-                                {
-                                    NavigationState navigationState = m_navHistory.Pop();
-                                    ReloadPage(navigationState.directory, navigationState.page);
-                                }
-                            }),
+                            onPressed: Back),
 
 
                         RaisedButton.icon(
